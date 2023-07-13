c9765675305894c4f7a4a0641513d34ac7931d9b
Priyansh Thakar <priyanshthakar@gmail.com>
2023-07-13 19:38:40 -0400

    Final Commit

--- js/coffee.js
+++ js/coffee.js
@@ -1,50 +1,91 @@
 const output = document.querySelector('#output');
 
 /* STEP 1: Instead of a constructor function, let's try a JavaScript class called 'Coffee' */
-
-// create constructor with keyword
-
-// add a serveIt method
-
-// Generate an IMG of the coffee ordered
-
-// Set the src path for the IMG element
-
-// Determine caffeine status of the coffee
-
-// Set the src attribute of the new IMG element
-
-// Set the src attribute of the new IMG element
-
-// Set the size of the cup SVG image based on this.size
-
-// Size the IMG in terms of its height based on above number from the switch
-
-// Generate a description of the coffee and put it into the IMG title attribute
-
-
-// Insert the new IMG element into the paragraph
-
-// Output all object member values
-for (const [key, value] of Object.entries(this)) {
-    console.log(`${key}: ${value}`);
+class Coffee {
+    size;
+    isDecaf;
+    // create constructor with keyword
+    constructor(size, isDecaf) {
+        this.size = size;
+        this.isDecaf = isDecaf;
+    };
+    // add a serveIt method
+    serveIt() {
+        // Generate an IMG of the coffee ordered
+        const cup = document.createElement("img");
+        // Set the src path for the IMG element
+        cup.setAttribute("src", "images/coffee-cup.svg");
+        // Determine caffeine status of the coffee
+        var decaf;
+        if (this.isDecaf === true) {
+            decaf = "decaffeinated";
+            // Set the src attribute of the new IMG element
+            cup.setAttribute("src", "images/coffee-cup-green.svg");
+        } else {
+            decaf = "caffeinated";
+            // Set the src attribute of the new IMG element
+            cup.setAttribute("src", "images/coffee-cup-purple.svg");
+        }
+        // Set the size of the cup SVG image based on this.size
+        var cupSize;
+        switch (this.size) {
+            case "small":
+                cupSize = 100;
+                break;
+            case "medium":
+                cupSize = 150;
+                break;
+            case "large":
+                cupSize = 200;
+                break;
+            default:
+                cupSize = 150;
+        }
+        // Size the IMG in terms of its height based on above number from the switch
+        cup.setAttribute("height", cupSize);
+        // Generate a description of the coffee and put it into the 
+        //IMG title attribute
+        var description = `A ${this.size} ${this.decaf} coffee`;
+        cup.setAttribute("title", description);
+        // Insert the new IMG element into the paragraph
+        output.appendChild(cup);
+        // Output all object member values
+        for (const [key, value] of Object.entries(this)) {
+            console.log(`${key}: ${value}`);
+        };
+    };
 };
-
-/* STEP 2: Instatiate a coffee based on the above constructor function */
+/* STEP 2: Instatiate a coffee based on the 
+above constructor function */
+let priyanshCoffee = new Coffee("medium", false);
 
 /* STEP 3: Add a method to the Coffee class called serveIt() */
 
 /* STEP 4: Call up the serveIt() method */
+priyanshCoffee.serveIt();
+let robertCoffee = new Coffee("small", true);
+robertCoffee.serveIt();
+let meganCoffee = new Coffee("large", true);
+meganCoffee.serveIt();
 
 /* STEP 5: Define a subclass of the Coffee class */
-
+class Latte extends Coffee {
+    milkType;
+    constructor(size, isDecaf, milkType) {
+        super(size, isDecaf); // this.size = size; this.isDecaf = isDecaf
+        this.milkType = milkType;
+    };
+    description() {
+        console.log(`A ${this.size} latte with ${this.milkType} milk.`);
+    }
+}
 /* STEP 6: Create a new instance of the Latte object */
-
+let priyanshLatte = new Latte("medium", true, "2%");
 /* STEP 7: Call up the latteDesc() method for the above created Latte instance */
 
 /* STEP 8: Create yet another instance of Latte using the console, 
-and try the latteDesc() method from the subclass, as well as the serveIt() method from the parent class */
-
+and try the latteDesc() method from the subclass, as well as the serveIt() method 
+from the parent class */
 
 /* LAB 07: Using the coffee.html file, create a new constructor for a different type of coffee (like an Americano, Espresso or ???). 
 Based on this new coffee on the Coffee object. 
